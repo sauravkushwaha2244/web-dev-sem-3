6e1120ce53428f65ab5e0d5fb59cf08e9dd8648c
sauravkushwaha2244 <sauriya2244@gmail.com>
2026-09-22 17:49:56 +0530

    assignment

---
+++ assignment_1/form1.js
@@ -0,0 +1,24 @@
+const button=document.querySelector('.btn1');
+
+button.addEventListener('click', function() {
+  window.open('q1.html', );
+});
+const button2=documnent.querySelector('.btn2');
+
+button2.addEventListener('click', function() {
+  window.open('q2.html', );
+});
+const button3=documnent.querySelector('.btn3');
+
+button3.addEventListener('click', function() {
+  window.open('q3.html', );
+});const button4=documnent.querySelector('.btn4');
+
+button4.addEventListener('click', function() {
+  window.open('q4.html', );
+});
+const button5=documnent.querySelector('.btn5');
+
+button5.addEventListener('click', function() {
+  window.close('q5.html', );
+});
